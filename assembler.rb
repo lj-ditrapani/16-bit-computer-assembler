97a3f9c4cc8a19d65d10387a32876595c03deef3
Lyall Jonathan Di Trapani <lj.ditrapani@gmail.com>
2014-10-23 10:25:05 -0400

    make_symbol_table code

--- assembler.rb
+++ assembler.rb
@@ -61,7 +61,7 @@ module Assembler
     lines = File.readlines(ARGV[0]).to_a
     asm = Assembly.new lines
     commands = CommandList.new
-    symbol_table = {'R0' => 0, 'R1' => 1}
+    symbol_table = {:R0 => 0, :R1 => 1}
     new_lines = []
     # can't use each_with_index since the line_number and word_index can
     # change by a variable # based on the command
@@ -73,7 +73,7 @@ module Assembler
       cmd_str, args_str = line.split(/\s+/, 2)
       if cmd_str[0] == '('
         # handle label
-        symbol_table[line[1...-1]] = commands.word_index
+        symbol_table[line[1...-1].to_sym] = commands.word_index
       end
       if cmd_str == '.set'
         # handle .set
@@ -81,7 +81,7 @@ module Assembler
         value = begin
           Integer(str_value)
         rescue
-          symbol_table[str_value]
+          symbol_table[str_value.to_sym]
           # need error handling here!
         end
         symbol_table[name] = value
@@ -100,6 +100,36 @@ module Assembler
     else
       new_line
     end
+  end
+
+
+  def self.make_symbol_table
+    st = {
+      :sound => 0xD800,
+      :"net-in" => 0xDC00,
+      :"net-out" => 0xE000,
+      :"storage-in" => 0xE400,
+      :"storage-out" => 0xE800,
+      :tiles => 0xEC00,
+      :grid => 0xF400,
+      :"cell-x-y-flip" => 0xFD60,
+      :sprites => 0xFE8C,
+      :"cell-colors" => 0xFF8C,
+      :"sprite-colors" => 0xFFAC,
+      :keyboard => 0xFFFA,
+      :"net-status" => 0xFFFB,
+      :"enable-bits" => 0xFFFC,
+      :"storage-read-address" => 0xFFFD,
+      :"storage-write-address" => 0xFFFE,
+      :"frame-interrupt-vector" => 0xFFFF,
+    }
+    (0...16).each do |i|
+      st[("R" + i.to_s).to_sym] = i
+    end
+    ('A'..'F').each_with_index do |c, i|
+      st[("R" + c).to_sym] = i + 10
+    end
+    st
   end
 
 end
