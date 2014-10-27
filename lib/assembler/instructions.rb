96181e831e490e1524020ed2bd442c4f6bb98354
Lyall Jonathan Di Trapani <lj.ditrapani@gmail.com>
2014-10-27 12:30:48 -0400

    Create Assembler::Instructions::HBY class

--- lib/assembler/instructions.rb
+++ lib/assembler/instructions.rb
@@ -1,4 +1,32 @@
 module Assembler::Instructions
-  def self.handle(op_code_symbol, args_str)
+
+  class HBY < Assembler::Command
+    def initialize(args_str)
+      super()
+      value_str, register = args_str.split
+      @value = Assembler::Token.new value_str
+      @register = Assembler::Token.new register
+    end
+
+    def machine_code(symbol_table)
+      value = if @value.type == :int
+                @value.value
+              else
+                symbol_table[@value.value]
+              end
+      register = if @register.type == :int
+                   @register.value
+                 else
+                   symbol_table[@register.value]
+                 end
+      [1 << 12 | value << 4 | register]
+    end
   end
+
+  def self.handle(op_code_symbol, args_str)
+    if op_code_symbol == :HBY
+      const_get(op_code_symbol).new args_str
+    end
+  end
+
 end
