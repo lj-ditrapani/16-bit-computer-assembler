bc54ba5ca16b90bdb0e3817b829dae9845726a8a
Lyall Jonathan Di Trapani <lj.ditrapani@gmail.com>
2014-11-02 16:55:45 -0500

    Implement LBY class

--- lib/assembler/instructions.rb
+++ lib/assembler/instructions.rb
@@ -23,8 +23,31 @@ module Assembler::Instructions
     end
   end
 
+  class LBY < Assembler::Command
+    def initialize(args_str)
+      super()
+      value_str, register = args_str.split
+      @value = Assembler::Token.new value_str
+      @register = Assembler::Token.new register
+    end
+
+    def machine_code(symbol_table)
+      value = if @value.type == :int
+                @value.value
+              else
+                symbol_table[@value.value]
+              end
+      register = if @register.type == :int
+                   @register.value
+                 else
+                   symbol_table[@register.value]
+                 end
+      [2 << 12 | value << 4 | register]
+    end
+  end
+
   def self.handle(op_code_symbol, args_str)
-    if op_code_symbol == :HBY
+    if [:HBY, :LBY].include? op_code_symbol
       const_get(op_code_symbol).new args_str
     end
   end
