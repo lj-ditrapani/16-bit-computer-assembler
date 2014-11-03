58931e75720d6d3d902148607337c8711d888c48
Lyall Jonathan Di Trapani <lj.ditrapani@gmail.com>
2014-11-03 12:09:43 -0500

    Fix typo; change .word to .move

--- spec/directives_spec.rb
+++ spec/directives_spec.rb
@@ -8,10 +8,10 @@ describe Assembler::Directives do
   ]
   symbol_table = { :audio => 0xD800 }
   tests.each do |args_str, word_index, word_length|
-    describe ".word Directive" do
-      it ".word #{args_str} -> array of #{word_length} zeros" do
+    describe ".move Directive" do
+      it ".move #{args_str} -> array of #{word_length} zeros" do
         d = Assembler::Directives
-        cmd = d.handle(:".word", args_str, [], word_index, symbol_table)
+        cmd = d.handle(:".move", args_str, [], word_index, symbol_table)
         machine_code = cmd.machine_code symbol_table
         assert_equal word_length, cmd.word_length
         assert_equal word_length, machine_code.length
