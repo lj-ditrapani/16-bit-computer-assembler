7ab5b5a8d9ea52c2eaecd30435d33021758f0b08
Lyall Jonathan Di Trapani <lj.ditrapani@gmail.com>
2014-11-26 10:46:47 -0500

    Add pseudo_intructions_spec to test script

--- test.sh
+++ test.sh
@@ -1,5 +1,6 @@
-rubocop lib/assembler.rb
+#rubocop lib/assembler.rb
 ruby spec/acceptance_spec.rb
 ruby spec/assembler_spec.rb
 ruby spec/directives_spec.rb
 ruby spec/instructions_spec.rb
+ruby spec/pseudo_instructions_spec.rb
