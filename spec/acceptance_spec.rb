1f05b6f5aba33eaf77e569ca127441f32a88a36d
Lyall Jonathan Di Trapani <lj.ditrapani@gmail.com>
2014-11-26 20:47:13 -0500

    Correct rubocop offenses

--- spec/acceptance_spec.rb
+++ spec/acceptance_spec.rb
@@ -1,22 +1,20 @@
 require 'minitest/autorun'
 require 'fileutils'
 
-
 def run_cli_test(name, qualified_name)
   asm_file = "#{qualified_name}.asm"
   exe_file = "#{qualified_name}.exe"
   exe_path = "actual-executables/#{exe_file}"
   expected_exe_path = "expected-executables/#{name}.exe"
   describe "When the #{asm_file} program is given as CL input" do
-    %x(./bin/assembler assembly-programs/#{asm_file} > #{exe_path})
+    `./bin/assembler assembly-programs/#{asm_file} > #{exe_path}`
     it "Should produce the #{exe_file} file as output" do
       assert FileUtils.cmp(exe_path, expected_exe_path)
     end
   end
 end
 
-
-describe "Assembler CLI" do
+describe 'Assembler CLI' do
   program_names = %w(adding branching while-loop)
   program_names.each do |name|
     run_cli_test name, "#{name}.no-symbols"
