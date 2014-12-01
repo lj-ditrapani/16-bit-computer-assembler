23085828d81a8414e6848f81ef82b1b0fad72cbe
Lyall Jonathan Di Trapani <lj.ditrapani@gmail.com>
2014-12-01 07:18:10 -0500

    Add additional acceptance tests
    To test NOP .str, .long-string, .end-long-string, .include and .copy

--- spec/acceptance_spec.rb
+++ spec/acceptance_spec.rb
@@ -1,14 +1,14 @@
 require 'minitest/autorun'
 require 'fileutils'
 
-def run_cli_test(name, qualified_name)
-  asm_file = "#{qualified_name}.asm"
-  exe_file = "#{qualified_name}.exe"
+def run_cli_test(name, expected_exe)
+  asm_file = "#{name}.asm"
+  exe_file = "#{name}.exe"
   exe_path = "actual-executables/#{exe_file}"
-  expected_exe_path = "expected-executables/#{name}.exe"
+  expected_exe_path = "expected-executables/#{expected_exe}.exe"
   describe "When the #{asm_file} program is given as CL input" do
     `./bin/assembler assembly-programs/#{asm_file} > #{exe_path}`
-    it "Should produce the #{exe_file} file as output" do
+    it "Should produce the #{expected_exe} file as output" do
       assert FileUtils.cmp(exe_path, expected_exe_path)
     end
   end
@@ -17,7 +17,14 @@ end
 describe 'Assembler CLI' do
   program_names = %w(adding branching while-loop)
   program_names.each do |name|
-    run_cli_test name, "#{name}.no-symbols"
-    run_cli_test name, "#{name}.symbols"
+    run_cli_test "#{name}.no-symbols", name
+    run_cli_test "#{name}.symbols", name
+  end
+end
+
+describe 'Assembler CLI' do
+  program_names = %w(misc includer copier include-and-copy)
+  program_names.each do |name|
+    run_cli_test name, "misc"
   end
 end
