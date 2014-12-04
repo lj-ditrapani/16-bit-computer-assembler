ae9de11b54292edc9988984a3faaca52b43bab0d
Lyall Jonathan Di Trapani <lj.ditrapani@gmail.com>
2014-12-04 06:33:38 -0500

    Add a CPY instruction to nested-include.asm

--- assembly-programs/nested-include.asm
+++ assembly-programs/nested-include.asm
@@ -3,6 +3,7 @@ WRD storage-out RA
 WRD $1234 R0
 WRD $4321 R1
 ADD R0 R1 R2                    # R2 should have $5555
+CPY R2 R3                       # Copy result into register 3
 # Will include another file
 .include assembly-programs/include-and-copy.asm
-STR RA R2                       # Store the output
+STR RA R3                       # Store the output
