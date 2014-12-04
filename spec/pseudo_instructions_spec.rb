04cf3bcf9c072b2ef8099e97b6fc21a33b431e8a
Lyall Jonathan Di Trapani <lj.ditrapani@gmail.com>
2014-12-04 06:31:31 -0500

    Add CPY pseudo-instruction spec

--- spec/pseudo_instructions_spec.rb
+++ spec/pseudo_instructions_spec.rb
@@ -9,3 +9,20 @@ describe Assembler::PseudoInstructions::NOP do
     assert_equal [0x7000], actual_machine_code
   end
 end
+
+describe Assembler::PseudoInstructions::CPY do
+  tests = [
+    ['R1 R2', 0x7102],
+    ['$F $A', 0x7F0A],
+    ['%0100 $5', 0x7405]
+  ]
+  symbol_table = { R1: 1, R2: 2 }
+  tests.each do |args_str, word|
+    it "CPY #{args_str} -> [#{word}]" do
+      cmd = Assembler::PseudoInstructions.handle(:CPY, args_str)
+      actual_machine_code = cmd.machine_code(symbol_table)
+      assert_equal 1, cmd.word_length
+      assert_equal [word], actual_machine_code
+    end
+  end
+end
