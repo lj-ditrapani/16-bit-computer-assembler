0b6272c595b344af4f1209a14e8fcaca1af40c36
Lyall Jonathan Di Trapani <lj.ditrapani@gmail.com>
2014-12-07 15:26:01 -0500

    Update test script

--- test.sh
+++ test.sh
@@ -1,7 +1,8 @@
 # rubocop lib
 rubocop spec
-ruby spec/acceptance_spec.rb
 ruby spec/assembler_spec.rb
 ruby spec/directives_spec.rb
 ruby spec/instructions_spec.rb
 ruby spec/pseudo_instructions_spec.rb
+ruby spec/acceptance_spec.rb
+ruby spec/failure_spec.rb
