7afd57ea0f8164628aadf2ed19efabd5f85fe063
Lyall Jonathan Di Trapani <lj.ditrapani@gmail.com>
2014-12-07 21:13:04 -0500

    Create SymbolTable class

--- lib/assembler/base.rb
+++ lib/assembler/base.rb
@@ -47,6 +47,60 @@ module Assembler
     end
   end
 
+  # Mapping from symbols to integers
+  class SymbolTable < Hash
+    def self.add_register_symbols
+      add_hex_register_symbols(add_decimal_register_symbols(BASE.dup))
+    end
+
+    def self.add_decimal_register_symbols(base)
+      (0...16).each do |i|
+        base[('R' + i.to_s).to_sym] = i
+      end
+      base
+    end
+
+    def self.add_hex_register_symbols(base)
+      ('A'..'F').each_with_index do |c, i|
+        base[('R' + c).to_sym] = i + 10
+      end
+      base
+    end
+
+    BASE = {
+      :audio => 0xD800,
+      :"net-in" => 0xDC00,
+      :"net-out" => 0xE000,
+      :"storage-in" => 0xE400,
+      :"storage-out" => 0xE800,
+      :tiles => 0xEC00,
+      :grid => 0xF400,
+      :"cell-x-y-flip" => 0xFD60,
+      :sprites => 0xFE8C,
+      :"cell-colors" => 0xFF8C,
+      :"sprite-colors" => 0xFFAC,
+      :keyboard => 0xFFFA,
+      :"net-status" => 0xFFFB,
+      :"enable-bits" => 0xFFFC,
+      :"storage-read-address" => 0xFFFD,
+      :"storage-write-address" => 0xFFFE,
+      :"frame-interrupt-vector" => 0xFFFF
+    }
+
+    SYMBOL_TABLE = add_register_symbols
+
+    def initialize
+      super
+      merge! SYMBOL_TABLE
+    end
+
+    def [](key)
+      value = super
+      fail Assembler::AsmError, "Undefined symbol: #{key.inspect}" if value.nil?
+      value
+    end
+  end
+
   # Holds the list of assembly lines yet to be processed
   class Assembly
     attr_reader :line_number
@@ -94,7 +148,7 @@ module Assembler
     end
 
     def word_length
-      @word_index
+      word_index
     end
 
     def machine_code(symbol_table)
