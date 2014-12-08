0aa4068641eede6aa4548c901dfe216a94f188b0
Lyall Jonathan Di Trapani <lj.ditrapani@gmail.com>
2014-12-07 21:13:26 -0500

    Use SymbolTable class instead of basic Hash

--- lib/assembler.rb
+++ lib/assembler.rb
@@ -58,32 +58,7 @@ module Assembler
   end
 
   def self.make_symbol_table
-    st = {
-      :audio => 0xD800,
-      :"net-in" => 0xDC00,
-      :"net-out" => 0xE000,
-      :"storage-in" => 0xE400,
-      :"storage-out" => 0xE800,
-      :tiles => 0xEC00,
-      :grid => 0xF400,
-      :"cell-x-y-flip" => 0xFD60,
-      :sprites => 0xFE8C,
-      :"cell-colors" => 0xFF8C,
-      :"sprite-colors" => 0xFFAC,
-      :keyboard => 0xFFFA,
-      :"net-status" => 0xFFFB,
-      :"enable-bits" => 0xFFFC,
-      :"storage-read-address" => 0xFFFD,
-      :"storage-write-address" => 0xFFFE,
-      :"frame-interrupt-vector" => 0xFFFF
-    }
-    (0...16).each do |i|
-      st[('R' + i.to_s).to_sym] = i
-    end
-    ('A'..'F').each_with_index do |c, i|
-      st[('R' + c).to_sym] = i + 10
-    end
-    st
+    Assembler::SymbolTable.new
   end
 
   def self.to_int(str)
