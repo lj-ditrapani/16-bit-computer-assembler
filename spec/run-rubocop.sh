eeb7b8c4acbb1972a6dbf820fbf91880abdb53d3
Lyall Jonathan Di Trapani <lj.ditrapani@gmail.com>
2014-12-17 06:55:03 -0500

    Add directives.rb to rubocop checks

--- spec/run-rubocop.sh
+++ spec/run-rubocop.sh
@@ -1,4 +1,5 @@
 # rubocop lib
 rubocop spec lib/assembler/pseudo_instructions.rb \
   lib/assembler/base.rb \
-  lib/assembler/instructions.rb
+  lib/assembler/instructions.rb \
+  lib/assembler/directives.rb
