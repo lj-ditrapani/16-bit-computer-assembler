98f1a50138d3d97f55e6c5fafe06aad2de7be4c9
Lyall Jonathan Di Trapani <lj.ditrapani@gmail.com>
2014-12-21 15:44:36 -0500

    Create an Assembler class and refactor
    Many Assembler module methods took a combination of source, commands,
    and symbol_table as arguments.  The Assembler class makes these instance
    variables so the instance methods' argument lists are much shorter.

--- lib/assembler.rb
+++ lib/assembler.rb
@@ -5,39 +5,67 @@ require './lib/assembler/pseudo_instructions'
 
 # All code for project contained within this top-level module
 module Assembler
-  def self.main(file_path)
-    source, commands, symbol_table = init_state(file_path)
-    begin
-      # can't use each_with_index since the line_number and word_index
-      # can change by a variable # based on the command
-      process_next_line(source, symbol_table, commands) until source.empty?
+  # Has main algorithm and state of source, commands, and symbol_table
+  class Assembler
+    def initialize(file_path)
+      @source = Source.new.include_file(file_path)
+      @commands, @symbol_table = CommandList.new, SymbolTable.new
+    end
+
+    def assemble
+      process_next_line until @source.empty?
     rescue AsmError => e
-      elaborate_error(e, source.current_line.source_info)
+      ::Assembler.elaborate_error(e, @source.current_line.source_info)
     else
-      machine_code(commands, symbol_table)
+      machine_code
+    end
+
+    def process_next_line
+      line = @source.pop_line
+      return if line.empty?
+      line.word_index = @commands.word_index
+      dispatch line
+    end
+
+    def machine_code
+      machine_code_arr = @commands.machine_code @symbol_table
+      machine_code_arr.pack('S>*')
+    end
+
+    private
+
+    def dispatch(line)
+      a = ::Assembler
+      args_str = line.args_str
+      case a.line_type(line.first_word)
+      when :label then a.label(line, @symbol_table)
+      when :set_directive then a.set_directive(args_str, @symbol_table)
+      when :include_directive then @source.include_file args_str
+      when :command then handle_command(line)
+      end
+    end
+
+    def handle_command(line)
+      command = create_command(line)
+      command.source_info = line.source_info
+      @commands.add_command command
+    end
+
+    def create_command(line)
+      first_sym, args_str = line.first_word.to_sym, line.args_str
+      if first_sym.to_s[0] == '.'
+        Directives.handle(line, @source, @symbol_table)
+      elsif ::Assembler.pseudo_instruction? first_sym
+        PseudoInstructions.handle(first_sym, args_str)
+      else
+        Instructions.handle(first_sym, args_str)
+      end
     end
   end
 
-  def self.init_state(file_path)
-    source = Source.new.include_file(file_path)
-    [source, CommandList.new, SymbolTable.new]
-  end
-
-  def self.process_next_line(source, symbol_table, commands)
-    line = source.pop_line
-    return if line.empty?
-    line.word_index = commands.word_index
-    dispatch(line, source, symbol_table, commands)
-  end
-
-  def self.dispatch(line, source, symbol_table, commands)
-    args_str = line.args_str
-    case line_type(line.first_word)
-    when :label then label(line, symbol_table)
-    when :set_directive then set_directive(args_str, symbol_table)
-    when :include_directive then source.include_file args_str
-    when :command then handle_command(line, source, commands, symbol_table)
-    end
+  def self.main(file_path)
+    asm = Assembler.new file_path
+    print asm.assemble
   end
 
   def self.elaborate_error(error, source_info)
@@ -46,12 +74,6 @@ module Assembler
                 ["#{error.message}",
                  "#{error.backtrace.join "\n    "}\n****\n\n"]
     $stderr.print msg_lines.join("\n")
-  end
-
-  def self.machine_code(commands, symbol_table)
-    machine_code_arr = commands.machine_code symbol_table
-    machine_code_str = machine_code_arr.pack('S>*')
-    print machine_code_str
   end
 
   def self.to_int(str)
@@ -98,25 +120,8 @@ module Assembler
     symbol_table.set_token(name, token)
   end
 
-  def self.handle_command(line, source, commands, symbol_table)
-    command = create_command(line, source, symbol_table)
-    command.source_info = line.source_info
-    commands.add_command command
-  end
-
   def self.pseudo_instruction?(first_word)
     pseudo_instructions_list = [:CPY, :NOP, :WRD, :INC, :DEC, :JMP]
     pseudo_instructions_list.include? first_word
-  end
-
-  def self.create_command(line, source, symbol_table)
-    first_sym, args_str = line.first_word.to_sym, line.args_str
-    if first_sym.to_s[0] == '.'
-      Directives.handle(line, source, symbol_table)
-    elsif pseudo_instruction? first_sym
-      PseudoInstructions.handle(first_sym, args_str)
-    else
-      Instructions.handle(first_sym, args_str)
-    end
   end
 end
