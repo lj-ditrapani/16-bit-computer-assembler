5eed6ea5ca72a1c3e5d9854dbfe71208946a7805
Lyall Jonathan Di Trapani <lj.ditrapani@gmail.com>
2014-12-21 21:59:45 -0500

    Make label and set_directives private methods

--- lib/assembler.rb
+++ lib/assembler.rb
@@ -38,9 +38,9 @@ module Assembler
       a = ::Assembler
       args_str = line.args_str
       case a.line_type(line.first_word)
-      when :label then a.label(line, @symbol_table)
-      when :set_directive then a.set_directive(args_str, @symbol_table)
-      when :include_directive then @source.include_file args_str
+      when :label then handle_label(line)
+      when :set_directive then handle_set_directive(args_str)
+      when :include_directive then handle_include_directive(args_str)
       when :command then handle_command(line)
       end
     end
@@ -61,6 +61,20 @@ module Assembler
         Instructions.handle(first_sym, args_str)
       end
     end
+
+    def handle_label(line)
+      @symbol_table[line.first_word[1...-1]] = line.word_index
+    end
+
+    def handle_set_directive(args_str)
+      name, str_value = args_str.split(/\s+/, 2)
+      token = Token.new str_value
+      @symbol_table.set_token(name, token)
+    end
+
+    def handle_include_directive(args_str)
+      @source.include_file args_str
+    end
   end
 
   def self.main(file_path)
@@ -78,15 +92,5 @@ module Assembler
     else
       :command
     end
-  end
-
-  def self.label(line, symbol_table)
-    symbol_table[line.first_word[1...-1]] = line.word_index
-  end
-
-  def self.set_directive(args_str, symbol_table)
-    name, str_value = args_str.split(/\s+/, 2)
-    token = Token.new str_value
-    symbol_table.set_token(name, token)
   end
 end
