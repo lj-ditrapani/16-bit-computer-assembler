2e7471291d84e69088a20b407de54ef372324a8c
Lyall Jonathan Di Trapani <lj.ditrapani@gmail.com>
2014-12-22 18:14:43 -0500

    Always check for file name & line number in stderr

--- spec/failure_spec.rb
+++ spec/failure_spec.rb
@@ -7,18 +7,19 @@ def run_failing_cli_test(name, reg_ex, line_num = nil)
   describe "When the #{name}.asm program is given as CL input" do
     `./bin/assembler #{asm_path} 2> #{stderr_path}`
     it 'It outputs an AsmError on stderr' do
-      compare_stderr(stderr_path, reg_ex, line_num)
+      compare_stderr(stderr_path, reg_ex, asm_path, line_num)
     end
   end
 end
 
-def compare_stderr(stderr_path, reg_ex, line_num)
+def compare_stderr(stderr_path, reg_ex, asm_path, line_num)
   File.open(stderr_path) do |f|
     str = f.read
     assert_match reg_ex, str
-    assert_match("LINE # #{line_num}", str) if line_num
+    assert_match "ASSEMBLER ERROR in file #{asm_path}", str
+    assert_match("LINE # #{line_num}", str)
   end
 end
 
 run_failing_cli_test 'bad-int', /Malformed integer/, 2
-run_failing_cli_test 'bad-symbol', /Undefined symbol: :R16/
+run_failing_cli_test 'bad-symbol', /Undefined symbol: :R16/, 2
