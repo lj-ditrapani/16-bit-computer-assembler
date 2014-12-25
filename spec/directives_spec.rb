e45ee77c78bb9a83ef458cf8093308ed27c7eceb
Lyall Jonathan Di Trapani <lj.ditrapani@gmail.com>
2014-12-25 12:00:37 -0500

    Refactor Directive specs

--- spec/directives_spec.rb
+++ spec/directives_spec.rb
@@ -3,128 +3,120 @@ require './lib/assembler'
 
 describe Assembler::Directives do
   Source = Assembler::Source
-  symbol_table = { audio: 0xD800 }
-  handle = lambda do |directive, args_str, word_index, source, st|
+  SYMBOL_TABLE = { audio: 0xD800 }
+
+  def check(cmd, expected_machine_code)
+    actual_machine_code = cmd.machine_code SYMBOL_TABLE
+    length = expected_machine_code.length
+    assert_equal length, cmd.word_length
+    assert_equal length, actual_machine_code.length
+    assert_equal expected_machine_code, actual_machine_code
+  end
+
+  handle = lambda do |directive, args_str, word_index = 0, source = []|
     text = "#{directive}\t#{args_str}"
     line = Source::Line.new('f.asm', 5, text)
     line.word_index = word_index
     source = Source.new.include_lines(source) if source.class == Array
-    Assembler::Directives.handle(line, source, st)
+    Assembler::Directives.handle(line, source, SYMBOL_TABLE)
   end
-  tests = [
-    ['$00FF', 0x0010, 239],
-    ['audio', 0x005, (0xD800 - 5)]
-  ]
-  tests.each do |args_str, word_index, word_length|
-    describe '.move Directive' do
-      it ".move #{args_str} -> array of #{word_length} zeros" do
-        cmd = handle.call(:'.move', args_str, word_index, [], symbol_table)
-        machine_code = cmd.machine_code symbol_table
-        assert_equal word_length, cmd.word_length
-        assert_equal word_length, machine_code.length
-        assert_equal 0, machine_code[0]
-        assert_equal 0, machine_code[-1]
-      end
-    end
-  end
-  tests = [
-    ['42', 42],
-    ['audio', 0xD800]
-  ]
-  tests.each do |args_str, word|
-    describe '.word Directive' do
-      it ".word #{args_str} -> [#{word}]" do
-        cmd = handle.call(:'.word', args_str, 0, [], symbol_table)
-        machine_code = cmd.machine_code symbol_table
-        assert_equal 1, cmd.word_length
-        assert_equal 1, machine_code.length
-        assert_equal word, machine_code[0]
-      end
-    end
-  end
-  tests = [
-    ['[1 2 3]', [], [1, 2, 3]],
-    ['[ 1 2 3', ['  4 5 6', '  7 8 9]'], [1, 2, 3, 4, 5, 6, 7, 8, 9]],
-    ['[', ["\t1", ' 2', "]\t"], [1, 2]],
-    [
-      '[$FFFF %0110_0000_1001_1111 64]',
-      [],
-      [0xFFFF, 0b0110_0000_1001_1111, 64]
+
+  describe '.move Directive' do
+    tests = [
+      ['$00FF', 0x0010, 239],
+      ['audio', 0x005, (0xD800 - 5)]
     ]
-  ]
-  tests.each do |args_str, lines, words|
-    describe '.array Directive' do
+    tests.each do |args_str, word_index, word_length|
+      it ".move #{args_str} -> array of #{word_length} zeros" do
+        cmd = handle.call(:'.move', args_str, word_index)
+        check(cmd, [0] * word_length)
+      end
+    end
+  end
+
+  describe '.word Directive' do
+    tests = [
+      ['42', 42],
+      ['audio', 0xD800]
+    ]
+    tests.each do |args_str, word|
+      it ".word #{args_str} -> [#{word}]" do
+        cmd = handle.call(:'.word', args_str)
+        check(cmd, [word])
+      end
+    end
+  end
+
+  describe '.array Directive' do
+    tests = [
+      ['[1 2 3]', [], [1, 2, 3]],
+      ['[ 1 2 3', ['  4 5 6', '  7 8 9]'], [1, 2, 3, 4, 5, 6, 7, 8, 9]],
+      ['[', ["\t1", ' 2', "]\t"], [1, 2]],
+      [
+        '[$FFFF %0110_0000_1001_1111 64]',
+        [],
+        [0xFFFF, 0b0110_0000_1001_1111, 64]
+      ]
+    ]
+    tests.each do |args_str, lines, words|
       it ".array #{args_str} -> #{words.inspect}" do
         source = Source.new.include_lines lines
-        cmd = handle.call(:'.array', args_str, 0, source, {})
-        machine_code = cmd.machine_code({})
-        length = words.length
-        assert_equal length, cmd.word_length
-        assert_equal length, machine_code.length
-        assert_equal words, machine_code
+        cmd = handle.call(:'.array', args_str, 0, source)
+        check(cmd, words)
         assert source.empty?
       end
     end
   end
-  tests = [
-    ['1 0', [0]],
-    ['3 42', [42, 42, 42]],
-    ['4 $FF', [0xFF, 0xFF, 0xFF, 0xFF]],
-    ['2 %1010_1100', [0xAC, 0xAC]],
-    ['3 audio', [0xD800, 0xD800, 0xD800]]
-  ]
-  tests.each do |args_str, words|
-    describe '.fill-array Directive' do
+
+  describe '.fill-array Directive' do
+    tests = [
+      ['1 0', [0]],
+      ['3 42', [42, 42, 42]],
+      ['4 $FF', [0xFF, 0xFF, 0xFF, 0xFF]],
+      ['2 %1010_1100', [0xAC, 0xAC]],
+      ['3 audio', [0xD800, 0xD800, 0xD800]]
+    ]
+    tests.each do |args_str, words|
       it ".fill-array #{args_str} -> #{words.inspect}" do
-        cmd = handle.call(:'.fill-array', args_str, 0, [], {})
-        machine_code = cmd.machine_code(symbol_table)
-        length = words.length
-        assert_equal length, cmd.word_length
-        assert_equal length, machine_code.length
-        assert_equal words, machine_code
+        cmd = handle.call(:'.fill-array', args_str)
+        check(cmd, words)
       end
     end
   end
-  tests = [
-    '', 'a', 'a ', "a \t", 'abc', 'a b c', 'a "b" c', 'Hellow World',
-    'She said "hi" '
-  ]
-  tests.each do |args_str|
-    describe '.str Directive' do
+
+  describe '.str Directive' do
+    tests = [
+      '', 'a', 'a ', "a \t", 'abc', 'a b c', 'a "b" c', 'Hellow World',
+      'She said "hi" '
+    ]
+    tests.each do |args_str|
       words = args_str.split('').map(&:ord)
       words.unshift words.size
       it ".str #{args_str} -> #{words.inspect}" do
-        cmd = handle.call(:'.str', args_str, 0, [], {})
-        machine_code = cmd.machine_code(symbol_table)
-        length = words.length
-        assert_equal length, cmd.word_length
-        assert_equal length, machine_code.length
-        assert_equal words, machine_code
+        cmd = handle.call(:'.str', args_str)
+        check(cmd, words)
       end
     end
   end
-  list = [
-    ['.end-long-string'],
-    [' a b ', '.end-long-string  # end'],
-    [' a', 'b ', ".end-long-string\t# end"],
-    ['a', "\t\"b\"  \t", 'c d', '.end-long-string']
-  ]
-  tests = list.map { |lines| ['keep-newlines', "\n", lines] } +
-          list.map { |lines| ['strip-newlines', '', lines] }
-  tests.each do |args_str, char, lines|
-    describe '.long-string Directive' do
+
+  describe '.long-string Directive' do
+    list = [
+      ['.end-long-string'],
+      [' a b ', '.end-long-string  # end'],
+      [' a', 'b ', ".end-long-string\t# end"],
+      ['a', "\t\"b\"  \t", 'c d', '.end-long-string']
+    ]
+    tests = list.map { |lines| ['keep-newlines', "\n", lines.dup] } +
+            list.map { |lines| ['strip-newlines', '', lines.dup] }
+    tests.each do |args_str, char, lines|
       source_lines = lines.dup
       str_lines = lines.dup
       str_lines.pop
       words = str_lines.join(char).split('').map(&:ord)
       words.unshift words.size
       it ".long-string #{args_str} #{lines} -> #{words.inspect}" do
-        cmd = handle.call(:'.long-string', args_str, 0, source_lines, {})
-        machine_code = cmd.machine_code(symbol_table)
-        length = words.length
-        assert_equal length, cmd.word_length
-        assert_equal length, machine_code.length
-        assert_equal words, machine_code
+        cmd = handle.call(:'.long-string', args_str, 0, source_lines)
+        check(cmd, words)
       end
     end
   end
