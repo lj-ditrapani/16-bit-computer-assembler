342c1de386d1b030b082e1fff0aa63146f32c0fd
Lyall Jonathan Di Trapani <lj.ditrapani@gmail.com>
2014-12-28 08:04:03 -0500

    Add SHF and BRN specs
    Some basics specs as well as several specs to ensure AsmErrors are
    properly raised on invalid arguments.

--- spec/instructions_spec.rb
+++ spec/instructions_spec.rb
@@ -1,6 +1,9 @@
 require 'minitest/autorun'
 require './lib/assembler'
 
+symbol_table = Assembler::SymbolTable.new
+symbol_table.merge!(too_big_4: 16, x: 7, y: 15)
+
 describe Assembler::Instructions do
   tests = [
     [:END, '', 0x0000],
@@ -24,15 +27,18 @@ describe Assembler::Instructions do
     [:SHF, 'RB L 1 RC', 0xDB0C],
     [:SHF, 'RB L 8 RC', 0xDB7C],
     [:BRN, 'RC NZ RA', 0xECA6],
+    [:BRN, 'RC ZN RA', 0xECA6],
     [:BRN, 'RC NZP RA', 0xECA7],
+    [:BRN, 'RC PZN RA', 0xECA7],
+    [:BRN, 'RC PNZ RA', 0xECA7],
     [:BRN, 'RC ZP RA', 0xECA3],
+    [:BRN, 'RC PZ RA', 0xECA3],
     [:BRN, 'RC P RA', 0xECA1],
     [:BRN, '- RB', 0xE0B8],
     [:BRN, 'C RB', 0xE0B9],
     [:BRN, 'V RB', 0xE0BA],
     [:SPC, 'RA', 0xF00A]
   ]
-  symbol_table = { RA: 10, RB: 11, RC: 12, x: 7, y: 15 }
   tests.each do |op_code, args_str, expected_machine_code|
     describe op_code do
       str = "Given #{op_code} #{args_str}, "
@@ -52,6 +58,43 @@ describe Assembler::Instructions do
         Assembler::Instructions.handle(:BRN, 'N RA')
       end
       assert_match(/got "N" instead/, err.message)
+    end
+  end
+
+  describe 'Failing instructions raise AsmError' do
+    cond = "Invalid value condition, must be C V or -, received: '%s'"
+    tests = [
+      [:SHF, '16 R 7 RC', "Value must be less than 16: '16'"],
+      [:SHF, 'too_big_4 R 7 RC', "Value must be less than 16: '16'"],
+      [:SHF, 'RB R 7 16', "Value must be less than 16: '16'"],
+      [:SHF, 'RB R 7 too_big_4', "Value must be less than 16: '16'"],
+      [:SHF, 'R0 U 8 R1', "Direction must be L or R, recieved: 'U'"],
+      [:SHF, 'R0 8 R1', "Expected 4 arguments, received: 'R0 8 R1'"],
+      [:SHF, 'R0 U 8 R1 R2', "Expected 4 arguments, received: 'R0 U 8 R1 R2'"],
+      [:SHF, 'R0 L 0 R1', "Ammount must be greater than 0, recieved: '0'"],
+      [:SHF, 'R0 L 9 R1', "Ammount must be less than 9, recieved: '9'"],
+      [:BRN, 'RB ZP too_big_4', "Value must be less than 16: '16'"],
+      [:BRN, 'RB ZP 16', "Value must be less than 16: '16'"],
+      [:BRN, 'too_big_4 ZP RC', "Value must be less than 16: '16'"],
+      [:BRN, 'RC ZP RA RB', "Expected 3 arguments, received: 'RC ZP RA RB'"],
+      [:BRN, 'C RB RC', 'Undefined symbol: :"C"'],
+      [:BRN, 'C', "Expected 2 or 3 arguments, received: 'C'"],
+      [:BRN, 'R0 - R1',
+       "Invalid value condition, must be combination of NZP, received:  '-'"],
+      [:BRN, 'ZP RA', cond % 'ZP'],
+      [:BRN, 'CV RB', cond % 'CV'],
+      [:BRN, '-V RB', cond % '-V'],
+      [:BRN, 'C- RB', cond % 'C-'],
+      [:BRN, 'CV- RB', cond % 'CV-']
+    ]
+    tests.each do |mnemonic, args_str, error_msg|
+      it "#{mnemonic} #{args_str} -> raises #{error_msg}" do
+        err = assert_raises Assembler::AsmError do
+          cmd = Assembler::Instructions.handle(mnemonic, args_str)
+          cmd.machine_code symbol_table
+        end
+        assert_match error_msg, err.message
+      end
     end
   end
 
